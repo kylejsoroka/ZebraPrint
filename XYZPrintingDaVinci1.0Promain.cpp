afaa46389ed2afb4035a8b9f7ebb402410207738
kyle jozef soroka <42379466+kylejsoroka@users.noreply.github.com>
2018-10-03 14:20:32 -0400

    Update XYZPrintingDaVinci1.0Promain.cpp

--- XYZPrintingDaVinci1.0Promain.cpp
+++ XYZPrintingDaVinci1.0Promain.cpp
@@ -1,1 +1,67 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+
+#include <iostream>
+#include <cmath>
+#include <string>
+
+
+int main()
+{
+
+	class xyzproonepointtwo {
+		int nozzletemp, makerbotnum;
+	public:
+		void set_values(int, int);
+		int mrp(void);
+	};
+
+
+	class xyzproonepointtwoheatedbed {
+		int heatedbedtemp, bedarea, bedsize, locationy, locationz;
+	public:
+		void set_values(int, int, int);
+		int mrp(void);
+	};
+
+	class xyzproonepointtwonozzle {
+		int nozzletemp, locationy, locationz;
+	public:
+		void set_values(int, int, int);
+		int mrp(void);
+
+
+
+
+	};
+	class xyzproonepointtwosmartextruder {
+		int extrudertemp, smartextrudery, smartextruderz;
+	public:
+		void set_values(int, int, int);
+		int mrp(void);
+
+
+
+
+
+	};
+
+	class xyzproonepointtwoplafillament {
+		int fillemantplatype, platemp;
+	public:
+		void set_values(int, int);
+		int mrp(void);
+
+
+
+
+	};
+
+
+
+
+
+}
 
